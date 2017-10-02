e0c79c384548be9c3a9eda50d38755aa077be4a5
Oscar A. Ramirez <oscar.arturo.ramirez@gmail.com>
2017-10-02 18:18:30 -0400

    Firebase integration with DB and Auth

--- component/Style.js
+++ component/Style.js
@@ -7,6 +7,46 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
+  body: {
+    flex: 9,
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor: '#F5FCFF',
+  },
+  toolbar: {
+        height: 56,
+    backgroundColor: '#e9eaed',
+  },
+  textInput: {
+    height: 40,
+    width: 200,
+    borderColor: 'red',
+    borderWidth: 1
+  },
+  transparentButton: {
+    marginTop: 10,
+    padding: 15
+  },
+  transparentButtonText: {
+    color: '#0485A9',
+    textAlign: 'center',
+    fontSize: 16
+  },
+  primaryButton: {
+    margin: 10,
+    padding: 15,
+    backgroundColor: '#529ecc'
+  },
+  primaryButtonText: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 18
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
   cameraPreview: {
     flex: 1,
     justifyContent: 'flex-end',
