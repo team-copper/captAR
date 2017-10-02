4c4f79138b44c43dde5096e7605efe9e4138cac0
Anuj Pant <anuj.pant41@gmail.com>
2017-10-02 19:10:40 -0400

    updated package.json

--- android/app/src/main/java/com/captar/MainApplication.java
+++ android/app/src/main/java/com/captar/MainApplication.java
@@ -3,11 +3,9 @@ package com.captar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.bmustapha.reactlibrary.RNGeoFencePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.transistorsoft.rnbackgroundgeolocation.RNBackgroundGeolocation;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,11 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGeoFencePackage(),
             new VectorIconsPackage(),
             new MapsPackage(),
-            new RCTCameraPackage(),
-            new RNBackgroundGeolocation()
+            new RCTCameraPackage()
       );
     }
   };
