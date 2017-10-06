aa138d5c9b7f63ea3fbf0af076df8bc758f0c2b8
jordanjpark <jordanjpark@gmail.com>
2017-10-06 11:49:50 -0400

    SelectGameView now has button, action buttons are divided into two different menu buttons, SelectGameActionButton has component function space to create and join game

--- component/GameActionButton.js
+++ component/GameActionButton.js
@@ -1,16 +1,27 @@
 'use strict';
 
 import React, { Component } from 'react';
+import firebase from '../firebase';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { View } from 'react-native';
 import { Style } from './index';
 
 // default position state set as right-hander
-export default class ActionButtonView extends Component {
+export default class GameActionButtonView extends Component {
   constructor() {
     super();
     this.state = {position: 'right'};
+    this.onLogoutPress = this.onLogoutPress.bind(this);
+  }
+
+  onLogoutPress() {
+    firebase.auth().signOut()
+      .then(() => {
+        this.props.isLoggedOut();
+        this.props.navigate("LoginForm");
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -23,7 +34,7 @@ export default class ActionButtonView extends Component {
           <ActionButton.Item buttonColor='#1abc9c' title="Message" position={this.state.position} onPress={() => {}}>
             <Icon name="md-mail" style={Style.actionButtonIcon} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Log Out" position={this.state.position} onPress={() => this.props.onLogoutPress()}>
+          <ActionButton.Item buttonColor='#778899' title="Log Out" position={this.state.position} onPress={() => this.onLogoutPress()}>
           <Icon name="ios-log-out" style={Style.actionButtonIcon} />
         </ActionButton.Item>
         </ActionButton>
