0ea07e8af4d47873fc582a4a4cff18ad2b2d3cd0
jordanjpark <jordanjpark@gmail.com>
2017-10-06 16:09:10 -0400

    camera view can be now closed by a button on top right corner

--- component/Camera.js
+++ component/Camera.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import firebase from "../firebase";
 import { connect } from 'react-redux'
+import { View, TouchableOpacity, Text } from 'react-native';
 import Camera from 'react-native-camera';
 import { Style } from './index';
 import { isLoggedOut } from '../store'
@@ -22,12 +23,15 @@ class CameraView extends Component {
 
   render() {
     return (
-      <Camera
-        ref={(cam) => { this.camera = cam; }}
-        style={Style.cameraPreview}
-        aspect={Camera.constants.Aspect.fill}
-        orientation={Camera.constants.Orientation.auto}>
-      </Camera>
+        <Camera
+          ref={(cam) => { this.camera = cam; }}
+          style={Style.cameraPreview}
+          aspect={Camera.constants.Aspect.fill}
+          orientation={Camera.constants.Orientation.auto}>
+          <TouchableOpacity onPress={this.props.onCloseCamera}>
+            <Text style={Style.closeCameraContainer}> X</Text>
+          </TouchableOpacity>
+        </Camera>
     )
   }
 }
