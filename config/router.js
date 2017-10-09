deff686c516847ad24b3f54e4a343e06fdc1e12c
jordanjpark <jordanjpark@gmail.com>
2017-10-09 15:39:14 -0400

    fixed button to right

--- config/router.js
+++ config/router.js
@@ -9,20 +9,20 @@ import { LoginForm, GameView, SelectGameView } from '../component';
 // import {SelectGameView} from '../component/SelectGameView';
 
 export const GameScreen = StackNavigator({
-    LoginForm: {
-        screen: LoginForm,
-        navigationOptions: {
-            title: 'Sign-Up/Login',
-            header: null
-        }
-    },
-    SelectGameView: {
-        screen: SelectGameView,
-        navigationOptions: {
-            title: 'SelectGameView',
-            header: null
-        }
-    },
+    // LoginForm: {
+    //     screen: LoginForm,
+    //     navigationOptions: {
+    //         title: 'Sign-Up/Login',
+    //         header: null
+    //     }
+    // },
+    // SelectGameView: {
+    //     screen: SelectGameView,
+    //     navigationOptions: {
+    //         title: 'SelectGameView',
+    //         header: null
+    //     }
+    // },
     GameView: {
         screen: GameView,
         navigationOptions: {
