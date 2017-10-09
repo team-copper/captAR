6cac28314bddc84a3d35a4f4c1a2efc8418cedfd
jordanjpark <jordanjpark@gmail.com>
2017-10-09 16:26:47 -0400

    working on firebase-redux

--- assets/playerMarkers.js
+++ assets/playerMarkers.js
@@ -1,20 +1,20 @@
 "use strict";
 
-const redPlayer1 = require('../assets/redPlayer1.png');
+const redPlayer1 = require("../assets/redPlayer1.png");
 
-const redPlayer2 = require('../assets/redPlayer2.png');
+const redPlayer2 = require("../assets/redPlayer2.png");
 
-const redPlayer3 = require('../assets/redPlayer3.png');
+const redPlayer3 = require("../assets/redPlayer3.png");
 
-const redPlayer4 = require('../assets/redPlayer4.png');
+const redPlayer4 = require("../assets/redPlayer4.png");
 
-const bluePlayer1 = require('../assets/bluePlayer1.png');
+const bluePlayer1 = require("../assets/bluePlayer1.png");
 
-const bluePlayer2 = require('../assets/bluePlayer2.png');
+const bluePlayer2 = require("../assets/bluePlayer2.png");
 
-const bluePlayer3 = require('../assets/bluePlayer3.png');
+const bluePlayer3 = require("../assets/bluePlayer3.png");
 
-const bluePlayer4 = require('../assets/bluePlayer4.png');
+const bluePlayer4 = require("../assets/bluePlayer4.png");
 
 const playerMarkerPath = [
   redPlayer1,
--- store/player.js
+++ store/player.js
@@ -9,50 +9,7 @@ const CHANGE_PLAYER_STATUS = 'CHANGE_PLAYER_STATUS'
 const CLEAR_PLAYER = 'CLEAR_PLAYER'
 
 // Initial State (currently hardcoded)
-let players = [
-    {
-        session: {
-            gameId: 1,
-            duration: null, //do we need this? - jordan
-            // Re: we can work without it; only needed if we want the rounds to be 5 min max - jannine
-        },
-        playerId: 1,
-        location: { latitude: 40.703394, longitude: -74.008622 },
-        team: 'red',
-        // Question: after being tagged, instead of forcing a player to their own side, can we pause their game for 10 secs at their current loc?
-        hasFlag: false
-    },
-    {
-        session: {
-            gameId: 1,
-            duration: null, //do we need this?
-        },
-        playerId: 2,
-        location: { latitude: 40.703441, longitude: -74.008713 },
-        team: 'blue',
-        hasFlag: false
-    },
-    {
-        session: {
-            gameId: 1,
-            duration: null, //do we need this?
-        },
-        playerId: 3,
-        location: { latitude: 40.703325, longitude: -74.008456 },
-        team: 'red',
-        hasFlag: false
-    },
-    {
-        session: {
-            gameId: 1,
-            duration: null, //do we need this?
-        },
-        playerId: 4,
-        location: { latitude: 40.703258, longitude:  -74.008663 },
-        team: 'blue',
-        hasFlag: true
-    }
-]
+let players = []
 
 // HAVE SESSION/GAME ID on every object, duration, gameID
 
